3ec424b71a37fe4daa37866979ec2b5906cf7e27
carlosmsx <carlosmsx@gmail.com>
2022-08-15 11:16:15 -0300

    agregado componente Spinner

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import ListaNoticias from './ListaNoticias';
 import { Form, Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
+import Spinner from './Spinner'
 
 const Formulario = () => {
     //https://newsapi.org/
@@ -11,6 +12,7 @@ const Formulario = () => {
     const apiKey = 'b73be4b9b9ff4a54b30c97e0f4f98766';
 
     const [listaNoticias, setListaNoticias] = useState([]);
+    const [mostrarSpinner, setMostrarSpinner] = useState(false);
 
     const consultarAPI = async (category)=>{
         //setMostratSpinner(true);
@@ -23,6 +25,8 @@ const Formulario = () => {
     const change = (event)=>{
         consultarAPI(event.target.value);
     }
+
+    const mostrarComponente = (mostrarSpinner===true)?<Spinner/>:<ListaNoticias listaNoticias={listaNoticias}/>;
 
     return (
         <div className="pt-4">
@@ -42,7 +46,7 @@ const Formulario = () => {
                 </Form.Group>
             </Form>
             <hr/>
-            <ListaNoticias listaNoticias={listaNoticias}/>
+            {mostrarComponente}
         </div>
     );
 };
